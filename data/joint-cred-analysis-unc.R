b176393206cc8ee93467cefd5e960f474b3e47af
joshuaalley <jkalley14@tamu.edu>
2019-12-19 09:45:38 -0600

    add analysis w/ measurement uncertainty

---
+++ data/joint-cred-analysis-unc.R
@@ -0,0 +1,71 @@
+# Joshua Alley
+# Texas A&M University
+# Analysis of Treaty depth accounting for uncertainty in the latent measure
+
+
+# Use brms to analyze multiple datasets and combine the results. 
+
+# create multiple datasets with results from the latent depth analysis
+
+
+# create an empty list of dataframes with all the variables
+brms.data.key <- select(atop.milsup, avg.democ, econagg.dum, uncond.milsup, 
+                                     fp.conc.index, num.mem, wartime, asymm,
+                                     low.kap.sc, begyr, non.maj.only) 
+
+# Create a list of dataframes 
+brms.data.unc <- vector(mode = "list", length = ncol(post.score[1, , ]))
+
+for(i in 1:ncol(post.score[1, , ])){
+  brms.data.unc[[i]] <- cbind.data.frame(brms.data.key, post.score[1, , i]) 
+  colnames(brms.data.unc[[i]])[11] <- "latent.depth"
+}
+
+# rescale depth column:
+brms.data.unc.rs <- lapply(brms.data.unc, function(data){
+  cbind.data.frame(data, (data[, 11] + 10) / (10 + max(data[, 11]) + .01))
+}
+)
+
+# rename depth column
+for(i in 1:length(brms.data.unc.rs)){
+  colnames(brms.data.unc.rs[[i]])[12] <- "latent.depth.rs"
+}
+
+# sample 10 at random for an initial run
+brms.data.unc.short <- sample(brms.data.unc.rs, size = 500, replace = FALSE)
+
+### run model: brm_multiple
+# uses priors and formula from earlier model
+
+# depth model: update formula
+bf.depth.unc <- brmsformula(latent.depth.rs ~ avg.democ + econagg.dum + uncond.milsup +
+                          fp.conc.index + num.mem + wartime + asymm +
+                          low.kap.sc + begyr + non.maj.only,
+                        #+ (1 | p | gr(begyr, dist = "gaussian")),
+                        center = TRUE) + Beta(link = "logit", link_phi = "log")
+depth.priors.unc <- set_prior("normal(0, 1)", class = "b", resp = "latentdepthrs") 
+# same uncond milsup model and priors as earlier analysis
+priors.unc <- depth.priors.unc + uncond.priors
+
+# run the model on all 1000 datasets
+system.time(
+brm.multivar.unc <- brm_multiple(bf.depth.unc + bf.uncond +
+                      set_rescor(FALSE), 
+                    data = brms.data.unc.short,
+                    prior = priors.unc,
+                    chains = 2, cores = 2)
+)
+
+# Examine results
+pp_check(brm.multivar.unc, resp = "latentdepthrs")
+summary(brm.multivar.unc)
+
+# rhat warning is the result of non-overlapping chains in the submodels
+round(brm.multivar.unc$rhats, 2) # rhats are fine in the submodels
+summary(brm.multivar.unc$rhats) # summary by parameters
+max(brm.multivar.unc$rhats) # maximum rhat is not problematic
+
+mediation(brm.multivar.unc, treatment = "non.maj.only", prob = .9)
+
+
--- data/joint-cred-analysis.R
+++ data/joint-cred-analysis.R
@@ -117,34 +117,4 @@ mediation(brm.multivar.dum, treatment = "non.maj.only", prob = .9)
 
 
 
-# tabulate the results
-# latent depth mean
-med.res.mean <- mediation(brm.multivar, treatment = "non.maj.only", prob = .9)
-xtable(med.res.mean) # will probably plot instead
-med.mean.plot  <- as_tibble(med.res.mean) %>% filter(effect == "direct" | effect == "indirect" | effect == "total") %>%
-                    ggplot(aes(y = effect, x = value)) +
-                     geom_vline(xintercept = 0) +
-                     geom_point(size = 3) +
-                     geom_errorbarh(aes(xmin = hdi.low, xmax = hdi.high,
-                      height = .1), size = 1) + 
-                     labs(x = "Estimated Effect", y = "Effect Type") +
-                     ggtitle("Mean Latent Depth") +
-                     theme_bw()
-med.mean.plot
 
-
-# Deep alliance dummy 
-med.res.dum <- mediation(brm.multivar.dum, treatment = "non.maj.only", prob = .9)
-xtable(med.res.dum) # will probably plot instead
-med.dum.plot  <- as_tibble(med.res.dum) %>% filter(effect == "direct" | effect == "indirect" | effect == "total") %>%
-  ggplot(aes(y = effect, x = value)) +
-  geom_vline(xintercept = 0) +
-  geom_point(size = 3) +
-  geom_errorbarh(aes(xmin = hdi.low, xmax = hdi.high,
-                     height = .1), size = 1) + 
-  labs(x = "Estimated Effect", y = "Effect Type") +
-  ggtitle("Deep Alliance Dummy") +
-  theme_bw()
-med.dum.plot
-
-multiplot.ggplot(med.mean.plot, med.dum.plot)
